4b1329f71c34107f941a92c97c6131602d1aa5c2
tobinio <Tobias.frischmann1@gmail.com>
2024-12-07 09:00:25 +0100

    day7

---
+++ src/day7.rs
@@ -0,0 +1,121 @@
+use crate::Day;
+use rayon::iter::IntoParallelRefIterator;
+use rayon::iter::ParallelIterator;
+use std::cmp::Ordering;
+use std::collections::{HashMap, HashSet};
+use std::ops::Not;
+use std::ptr::eq;
+
+pub struct Day7 {}
+impl Day<usize> for Day7 {
+    fn number() -> usize {
+        7
+    }
+
+    fn run_star1(file: String) -> usize {
+        let equations = parse_input(file);
+
+        equations
+            .iter()
+            .filter(|equation| equation.is_valid(false))
+            .map(|equation| equation.result)
+            .sum()
+    }
+
+    fn run_star2(file: String) -> usize {
+        let equations = parse_input(file);
+
+        equations
+            .iter()
+            .filter(|equation| equation.is_valid(true))
+            .map(|equation| equation.result)
+            .sum()
+    }
+}
+
+pub struct Equation {
+    result: usize,
+    values: Vec<usize>,
+}
+
+impl Equation {
+    fn is_valid(&self, allow_concatenation: bool) -> bool {
+        self.check_valid(self.values[0], 1, allow_concatenation)
+    }
+
+    fn check_valid(&self, temp_result: usize, index: usize, allow_concatenation: bool) -> bool {
+        if (index >= self.values.len()) {
+            return temp_result == self.result;
+        }
+
+        if self.check_valid(
+            temp_result * self.values[index],
+            index + 1,
+            allow_concatenation,
+        ) {
+            return true;
+        }
+
+        if self.check_valid(
+            temp_result + self.values[index],
+            index + 1,
+            allow_concatenation,
+        ) {
+            return true;
+        }
+
+        if allow_concatenation
+            && self.check_valid(
+                (temp_result.to_string() + &self.values[index].to_string())
+                    .parse()
+                    .unwrap(),
+                index + 1,
+                allow_concatenation,
+            )
+        {
+            return true;
+        }
+
+        false
+    }
+}
+fn parse_input(file: String) -> Vec<Equation> {
+    file.lines()
+        .map(|line| {
+            let mut split = line.split(':');
+
+            let result = split.next().unwrap().parse().unwrap();
+
+            let values = split
+                .next()
+                .unwrap()
+                .trim()
+                .split_whitespace()
+                .map(|num| num.parse().unwrap())
+                .collect();
+
+            Equation {
+                result: result,
+                values: values,
+            }
+        })
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day7::Day7;
+    use crate::Day;
+
+    #[test]
+    fn example_star1() {
+        let result = Day7::run_star1(Day7::get_example());
+        assert_eq!(result, 3749);
+    }
+
+    #[test]
+    fn example_star2() {
+        let result = Day7::run_star2(Day7::get_example());
+        assert_eq!(result, 11387);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crate::day4::Day4;
 use crate::day5::Day5;
 use crate::day6::Day6;
+use crate::day7::Day7;
 use std::fmt::{Debug, Display};
 use std::fs;
 use std::time::Instant;
@@ -11,11 +12,12 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 fn main() {
     let instant = Instant::now();
 
-    println!("{}", Day6::run_star2(Day6::get_file()));
+    println!("{}", Day7::run_star2(Day7::get_file()));
     println!("{:?}", instant.elapsed());
 }
 trait Day<T: Debug> {
